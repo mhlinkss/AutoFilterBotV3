e257eb5c7037eef613a423e46dca19954cf33004
mhlinkss <107099913+mhlinkss@users.noreply.github.com>
2022-06-12 11:55:44 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🌀 MH LINKS 🌀 ', url=f'https://t.me/+gyZFP-mFh7YyN2Q1')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
